8c9418162695ddbbe358f2e4d8e3657bf20cd1a5
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2022-12-12 14:50:55 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,22 @@
+const mysql = require('mysql');
+require('dotenv').config()
 
+var connection = mysql.createConnection({
+	host : process.env.DB_HOST,
+	database : process.env.DB_DATABASE_NAME,
+	user : process.env.DB_USER,
+	password : process.env.DB_PASSWORD
+});
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
